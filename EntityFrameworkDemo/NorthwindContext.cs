f6fb1a597b4277e6defec9e51610eb5270dc0d4b
Zeynep <zeynp.inan@3269@gmail.com>
2021-07-04 15:53:22 +0300

    Add project files.

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
